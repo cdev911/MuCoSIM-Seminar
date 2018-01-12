f03638ddeaff9c406f9b4ade552e84fae1152dd3
cdev <chaitanya.dev@fau.de>
2018-01-12 10:07:54 +0100

    python setup

--- plot_result.py
+++ plot_result.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
+x = []
+y = []
+with open('data/data_size1', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[1]))
+        y.append(row[2])
 
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
+plt.plot(x, y)
 plt.show()
+#
+# t = np.arange(0.0, 2.0, 0.01)
+# s = 1 + np.sin(2*np.pi*t)
+# plt.plot(t, s)
+#
+# plt.xlabel('time (s)')
+# plt.ylabel('voltage (mV)')
+# plt.title('About as simple as it gets, folks')
+# plt.grid(True)
+# plt.show()
